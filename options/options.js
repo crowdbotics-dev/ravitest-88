e5ac8fc14a831c1206b54b8b3177ef86c0d1a02b
Crowdbotics <engineering@crowdbotics.com>
2024-01-18 11:16:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ravitest-88",
+  url: "https://ravitest-88.botics.co",
+  api: "https://ravitest-88.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
